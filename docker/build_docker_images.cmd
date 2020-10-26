a32872585413d2ba2a17700e8a81706bf585faba
yingding <yingdingwang@yahoo.de>
2020-10-26 19:02:14 +0100

    update submit container

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,4 +1,6 @@
+# each tag will create a docker image
 docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.1 .
-docker build -f Dockerfile_master -t sdesilva26/spark_master -t sdesilva26/spark_master:0.0.2 .
-docker build -f Dockerfile_worker -t sdesilva26/spark_worker -t sdesilva26/spark_worker:0.0.2 .
-docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
+docker build -f Dockerfile_master -t yingding/spark_master .
+docker build -f Dockerfile_worker -t yingding/spark_worker .
+docker build -f Dockerfile_submit -t yingding/spark_submit .
+# docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
