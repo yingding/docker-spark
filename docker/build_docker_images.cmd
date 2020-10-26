2c77b7729bfefb1af99d3c417ab063ae6b4dd005
yingding <yingdingwang@yahoo.de>
2020-10-26 18:39:14 +0100

    updated base image for Apache Spark 3.0.1 and OpenJDK11

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,3 +1,4 @@
+docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.1 .
 docker build -f Dockerfile_master -t sdesilva26/spark_master -t sdesilva26/spark_master:0.0.2 .
 docker build -f Dockerfile_worker -t sdesilva26/spark_worker -t sdesilva26/spark_worker:0.0.2 .
 docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
--- docker/push_docker_images.cmd
+++ docker/push_docker_images.cmd
@@ -1,5 +1,5 @@
 docker push sdesilva26/spark_master:latest
-docker push  sdesilva26/spark_master:0.0.2
+docker push sdesilva26/spark_master:0.0.2
 docker push sdesilva26/spark_worker:latest
 docker push sdesilva26/spark_worker:0.0.2
 docker push sdesilva26/spark_submit:latest
