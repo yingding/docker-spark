23ea36abdaf25e30ae6c69b09b82279b1fc035d6
yingding <yingdingwang@yahoo.de>
2020-10-26 20:15:37 +0100

    update build commands

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,6 +1,8 @@
 # each tag will create a docker image
-docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.1 .
+# Folgende build create in repository yingding/spark_base a tag latest and a tag 0.0.1 two images
+# docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.1 .
+docker build -f Dockerfile_base -t yingding/spark_base .
 docker build -f Dockerfile_master -t yingding/spark_master .
 docker build -f Dockerfile_worker -t yingding/spark_worker .
 docker build -f Dockerfile_submit -t yingding/spark_submit .
-# docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
+
