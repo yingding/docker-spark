4906c7204f24ff0ba3278d3eeb06dc88051aadf3
yingding <yingdingwang@yahoo.de>
2020-10-29 21:17:46 +0100

    updated output of tini so that the passing SIGTERM to the tini first child process is visible in docker container log while docker stop is called.

--- docker/build_docker_container.cmd
+++ docker/build_docker_container.cmd
@@ -2,9 +2,16 @@
 docker network create --driver bridge spark-net-bridge
 
 # 2. Start Docker Master on local port 8090, and 4041 to get the master log and job log:
-docker run -dit --init --name spark-master --network spark-net-bridge -p 8090:8080 -p 4041:4040 yingding/spark_master:latest bash
+# since the ENTRYPOINT is the /tini , thus we don't need to call docker with additional --init, otherwise two tini process will be started.
+# docker run -dit --init --name spark-master --network spark-net-bridge -p 8090:8080 -p 4041:4040 yingding/spark_master:latest bash
+
+docker run -dit --name spark-master --network spark-net-bridge -p 8090:8080 -p 4041:4040 yingding/spark_master:latest bash
+
+# login to the container and run the following command to see the java process as PID 6 of /tini -v
+ps -fA
 
 # 3. Start Docker Worker on local port 8081:
+# docker run -dit --init --name spark-worker1 --network spark-net-bridge -p 8081:8081 yingding/spark_worker:latest bash
 docker run -dit --init --name spark-worker1 --network spark-net-bridge -p 8081:8081 yingding/spark_worker:latest bash
 
 ## Note ##
