c56b550c46dca2f964d59ebf59ea300b5aebb495
yingding <yingdingwang@yahoo.de>
2020-11-05 12:37:09 +0100

    update init.d script

--- docker/deployment/sparkd.sh
+++ docker/deployment/sparkd.sh
@@ -39,6 +39,11 @@
 DOCKER_ENGINE=docker
 COMPOSE_BINARY=docker-compose
 
+DESC=engine
+NAME=sparkd
+# master service name in docker-compose file
+SPARK_MASTER_NAME="spark-master"
+
 # Test existing command/binary
 # Reference: https://stackoverflow.com/questions/7522712/how-can-i-check-if-a-command-exists-in-a-shell-script/7522866#7522866
 if ! type "$DOCKER_ENGINE" > /dev/null; then
@@ -52,10 +57,67 @@ fi
 #  exit 0
 #fi
 
-if ! type "$DOCKER_ENGINE" > /dev/null; then
+if ! type "$COMPOSE_BINARY" > /dev/null; then
   echo "Could not find $COMPOSE_BINARY"
   exit 0
 fi
+
+# use log functions
+. /lib/lsb/init-functions
+
+set -e
+
+# more complicated way is to write a health check command:
+# https://stackoverflow.com/questions/48994189/how-to-check-if-all-docker-compose-up-services-started-successfully
+# https://docs.docker.com/engine/reference/builder/#healthcheck
+running_container() {
+  # init local variable with a Global Variable
+  service_name=$1
+  # https://serverfault.com/questions/789601/check-is-container-service-running-with-docker-compose/935674#935674
+  # docker-compose ps -q <service_name> will display the container ID no matter it's running or not, as long as it was created.
+  # docker ps shows only those that are actually running.
+  if [ -z `docker-compose ps -q $service_name` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $service_name)` ]; then
+    echo "No, $service_name is not running."
+    # 1 = false
+    return 1
+  else
+    echo "Yes, $service_name is running."
+    # 0 = true
+    return 0
+  fi
+}
+
+running() {
+  # Check if the docker containers is running
+  # check if the spark master docker container is running
+  if running_container $SPARK_MASTER_NAME ; then
+    # 0 = true
+    return 0
+  else
+    # 1 = false
+    return 1
+  fi
+}
+
+case "$1" in
+  start)
+    log_daemon_msg "Starting $DESC" "$NAME"
+    # Check if it's running first
+    if running ; then
+      log_progress_msg "apparently already running"
+      log_end_msg 0
+      exit 0
+    fi
+
+    ;;
+  *)
+    N=/etc/init.d/$NAME
+    echo "Usage: $N {start|stop|restart|status}" >&2
+    exit 1
+    ;;
+esac
+
+exit 0
 
 
 
