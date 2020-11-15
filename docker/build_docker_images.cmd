c226925ff186dba3c3a38a7bea0e547cf1e55782
yingding <yingdingwang@yahoo.de>
2020-11-15 01:51:20 +0100

    add ping to base container

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,6 +1,7 @@
 # Following build command create in the repository "yingding/spark_base" a tag latest and a tag 0.0.1 two images
 # docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.1 .
 # Every tag declared during the build process will create a docker image on the local disk
+# use "docker image rm <imageId1> <imageId2>" to remove images
 docker build -f Dockerfile_base -t yingding/spark_base .
 docker build -f Dockerfile_master -t yingding/spark_master .
 docker build -f Dockerfile_worker -t yingding/spark_worker .
