b7f9dff27df187a0fe3919b69e3ef4b5e4dcd982
yingding <yingdingwang@yahoo.de>
2020-11-15 19:17:54 +0100

    use the start-master und start-slave scripts

--- docker/scripts/master/entrypoint.sh
+++ docker/scripts/master/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Spark Master entrypoint.sh
 # exec gosu $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
-exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
+exec $SPARK_HOME/sbin/start-master.sh --properties-file $SPARK_HOME/conf/spark-defaults.conf
+# exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
--- docker/scripts/worker/entrypoint.sh
+++ docker/scripts/worker/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Spark Worker entrypoint.sh
 # exec gosu $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker -c $CORES -m $MEMORY spark://$MASTER_CONTAINER_NAME:7077
-exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker -c $CORES -m $MEMORY spark://$MASTER_CONTAINER_NAME:7077
+# exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker -c $CORES -m $MEMORY --properties-file $SPARK_HOME/conf/spark-defaults.conf spark://${MASTER_CONTAINER_NAME}:${MASTER_CONTAINER_PORT}
+
+exec $SPARK_HOME/sbin/start-slave.sh -c $CORES -m $MEMORY --properties-file $SPARK_HOME/conf/spark-defaults.conf spark://${MASTER_CONTAINER_NAME}:${MASTER_CONTAINER_PORT}
