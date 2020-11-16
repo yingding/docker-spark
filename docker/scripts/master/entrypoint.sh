d1f28bf03c9351f0d7552ef212ae3d6fba476d37
yingding <yingdingwang@yahoo.de>
2020-11-16 02:36:46 +0100

    add pom.xml file for the spark-mongo-driver, so that a jar dependency of spark-mongo-driver can be build and added to /spark/jars for the worker container

--- docker/scripts/master/entrypoint.sh
+++ docker/scripts/master/entrypoint.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # Spark Master entrypoint.sh
 # exec gosu $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
-exec $SPARK_HOME/sbin/start-master.sh --properties-file $SPARK_HOME/conf/spark-defaults.conf
-# exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
+
+# Doesn't work
+# exec $SPARK_HOME/sbin/start-master.sh --properties-file $SPARK_HOME/conf/spark-defaults.conf
+
+# python config
+# . "$SPARK_HOME/sbin/spark-config.sh"
+
+# spark-class is loading spark env
+# . "$SPARK_HOME/bin/load-spark-env.sh"
+
+exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master
--- docker/scripts/worker/entrypoint.sh
+++ docker/scripts/worker/entrypoint.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 # Spark Worker entrypoint.sh
 # exec gosu $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker -c $CORES -m $MEMORY spark://$MASTER_CONTAINER_NAME:7077
-# exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker -c $CORES -m $MEMORY --properties-file $SPARK_HOME/conf/spark-defaults.conf spark://${MASTER_CONTAINER_NAME}:${MASTER_CONTAINER_PORT}
 
-exec $SPARK_HOME/sbin/start-slave.sh -c $CORES -m $MEMORY --properties-file $SPARK_HOME/conf/spark-defaults.conf spark://${MASTER_CONTAINER_NAME}:${MASTER_CONTAINER_PORT}
+# python config
+# . "$SPARK_HOME/sbin/spark-config.sh"
+
+# spark-class is loading spark env
+# . "$SPARK_HOME/bin/load-spark-env.sh"
+
+exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker -c $CORES -m $MEMORY spark://${MASTER_CONTAINER_NAME}:${MASTER_CONTAINER_PORT}
+
+# Doesn't work
+# exec $SPARK_HOME/sbin/start-slave.sh -c $CORES -m $MEMORY --properties-file $SPARK_HOME/conf/spark-defaults.conf spark://${MASTER_CONTAINER_NAME}:${MASTER_CONTAINER_PORT}
