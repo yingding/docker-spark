e04bad4bf7d2e3f057869fc38150eb4c53bac83e
yingding <yingdingwang@yahoo.de>
2021-07-10 20:50:46 +0200

    updated to spark version 3.1.2, scala sbt version to 2.12.14, openjdk hotspot focal to 11.0.11_9

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -2,8 +2,8 @@
 # docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.1 .
 # Every tag declared during the build process will create a docker image on the local disk
 # use "docker image rm <imageId1> <imageId2>" to remove images
-docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.2 .
-docker build -f Dockerfile_master -t yingding/spark_master -t yingding/spark_master:0.0.2 .
-docker build -f Dockerfile_worker -t yingding/spark_worker -t yingding/spark_worker:0.0.2 .
-docker build -f Dockerfile_submit -t yingding/spark_submit -t yingding/spark_submit:0.0.2 .
+docker build -f Dockerfile_base -t yingding/spark_base -t yingding/spark_base:0.0.3 .
+docker build -f Dockerfile_master -t yingding/spark_master -t yingding/spark_master:0.0.3 .
+docker build -f Dockerfile_worker -t yingding/spark_worker -t yingding/spark_worker:0.0.3 .
+docker build -f Dockerfile_submit -t yingding/spark_submit -t yingding/spark_submit:0.0.3 .
 
